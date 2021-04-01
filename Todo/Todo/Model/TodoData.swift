a8300551c5ef232df5e7bc13cd7af49870d368a7
91oo0 <91oo0.mail@gmail.com>
2021-04-02 00:54:14 +0900

    imprement create and read method for Realm Database

--- Todo/Todo/Model/TodoData.swift
+++ Todo/Todo/Model/TodoData.swift
@@ -1,2 +1,35 @@
 import UIKit
 import RealmSwift
+
+class Task: Object {
+    dynamic var id          = 0     // ID
+    dynamic var title       = ""    // タイトル
+    dynamic var during      = 0     // 間隔
+    dynamic var lastDate    = ""    // 最終実行日
+    dynamic var taskDisable = ""    // 無効
+}
+
+class RealmDatabase {
+    
+    let realm = try! Realm()
+    
+    func createTask(data: Task) {
+        try! realm.write() {
+            realm.add(data)
+        }
+    }
+    
+    func readTask() -> [Task] {
+        let results  = realm.objects(Task.self)
+        var list: [Task] = []
+        
+        for result in results {
+            list.append(result)
+        }
+        return list
+    }
+    
+    private func setTaskId(data: Task) {
+//        let latestTask = realm.objects(Task.self).sorted(byKeyPath: "id").max()
+    }
+}
